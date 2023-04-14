213a9923402029b1bb7ab69012626074a81b7334
dlgywls1130 <dlgywls1130@gmail.com>
2023-04-15 01:38:38 +0900

    레이아웃 작업

--- js/main.js
+++ js/main.js
@@ -22,6 +22,69 @@ function getCurrentUrl() {
   
 
 
+
+// responsive menu
+
+const navbarMenu = document.getElementById("navbar");
+const burgerMenu = document.getElementById("burger");
+const overlayMenu = document.querySelector(".overlay");
+
+
+const toggleMenu = () => {
+    navbarMenu.classList.toggle("active");
+    overlayMenu.classList.toggle("active");
+};
+
+
+const collapseSubMenu = () => {
+    navbarMenu
+        .querySelector(".menu-dropdown.active .submenu")
+        .removeAttribute("style");
+    navbarMenu.querySelector(".menu-dropdown.active").classList.remove("active");
+};
+
+
+const toggleSubMenu = (e) => {
+    if (e.target.hasAttribute("data-toggle") && window.innerWidth <= 992) {
+        e.preventDefault();
+        const menuDropdown = e.target.parentElement;
+
+        // If Dropdown is Expanded, then Collapse It
+        if (menuDropdown.classList.contains("active")) {
+            collapseSubMenu();
+        } else {
+            // Collapse Existing Expanded Dropdown
+            if (navbarMenu.querySelector(".menu-dropdown.active")) {
+                collapseSubMenu();
+            }
+
+            // Expanded the New Dropdown
+            menuDropdown.classList.add("active");
+            const subMenu = menuDropdown.querySelector(".submenu");
+            subMenu.style.maxHeight = subMenu.scrollHeight + "px";
+        }
+    }
+};
+
+
+const resizeWindow = () => {
+    if (window.innerWidth > 992) {
+        if (navbarMenu.classList.contains("active")) {
+            toggleMenu();
+        }
+        if (navbarMenu.querySelector(".menu-dropdown.active")) {
+            collapseSubMenu();
+        }
+    }
+};
+
+// Initialize Event Listeners
+burgerMenu.addEventListener("click", toggleMenu);
+overlayMenu.addEventListener("click", toggleMenu);
+navbarMenu.addEventListener("click", toggleSubMenu);
+window.addEventListener("resize", resizeWindow);
+
+
 // tab
 
 const tabItem = document.querySelectorAll(".tab-container__item");
