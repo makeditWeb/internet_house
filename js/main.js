1007512328bb07261c1f075cc6351f94f42126c7
dlgywls1130 <dlgywls1130@gmail.com>
2023-04-15 22:34:08 +0900

    결합페이지 레이아웃

--- js/main.js
+++ js/main.js
@@ -156,3 +156,76 @@ $(document).ready(function(){
 })
 
 
+const priceLists = document.querySelectorAll('.price_view .price_list');
+const homePriceLists = document.querySelectorAll('.price_view_home .price_list');
+const combinationSpan = document.querySelector('.combination_span');
+const infoContentTitle = document.querySelector('.info_content_title h2');
+const productMonthPriceTb = document.querySelector('#product_month_price_tb');
+
+// 1번: price_view와 price_view_home에서 하나씩 선택했을 때 product_month_price_tb 첫번째 td 바꾸기
+// priceLists.forEach((priceList, index) => {
+//   priceList.addEventListener('click', () => {
+//     const selectedPrice = priceList.textContent.trim();
+//     const selectedHomePrice = homePriceLists[index].textContent.trim();
+//     combinationSpan.textContent = `${selectedPrice} - ${selectedHomePrice}`;
+//     const firstTds = productMonthPriceTb.querySelectorAll('tr td:first-child');
+//     firstTds.forEach((td) => {
+//       td.textContent = `${selectedPrice} ${td.textContent.split(' ')[1]}`;
+//     });
+//   });
+// });
+
+// 2번: price_view와 price_view_home에서 하나씩 선택했을 때 info_content_title 바꾸기
+// homePriceLists.forEach((priceList, index) => {
+//   priceList.addEventListener('click', () => {
+//     const selectedHomePrice = priceList.textContent.trim();
+//     const selectedPrice = priceLists[index].textContent.trim();
+//     combinationSpan.textContent = `${selectedPrice} - ${selectedHomePrice}`;
+//     infoContentTitle.textContent = `이용중인 SKT휴대폰이 없을실 때(${selectedPrice})`;
+//   });
+// });
+
+// 3번: combination_span 안에 선택한 price_view와 price_view_home의 price_list 텍스트 넣기
+priceLists.forEach((priceList, index) => {
+  priceList.addEventListener('click', () => {
+    const selectedPrice = priceList.textContent.trim();
+    const selectedHomePrice = homePriceLists[index].textContent.trim();
+    combinationSpan.textContent = `${selectedPrice} - ${selectedHomePrice}`;
+  });
+});
+
+homePriceLists.forEach((priceList, index) => {
+  priceList.addEventListener('click', () => {
+    const selectedHomePrice = priceList.textContent.trim();
+    const selectedPrice = priceLists[index].textContent.trim();
+    combinationSpan.textContent = `${selectedPrice} - ${selectedHomePrice}`;
+  });
+});
+
+
+
+// 선택시 border change
+const priceViewList = document.querySelectorAll('.price_view li');
+for (let i = 0; i < priceViewList.length; i++) {
+  priceViewList[i].addEventListener('click', function() {
+    for (let j = 0; j < priceViewList.length; j++) {
+      if (priceViewList[j] !== this) {
+        priceViewList[j].classList.remove('selected');
+      }
+    }
+    this.classList.add('selected');
+  });
+}
+
+
+const priceViewHomeList = document.querySelectorAll('.price_view_home li');
+for (let i = 0; i < priceViewHomeList.length; i++) {
+  priceViewHomeList[i].addEventListener('click', function() {
+    for (let j = 0; j < priceViewHomeList.length; j++) {
+      if (priceViewHomeList[j] !== this) {
+        priceViewHomeList[j].classList.remove('selected');
+      }
+    }
+    this.classList.add('selected');
+  });
+}
